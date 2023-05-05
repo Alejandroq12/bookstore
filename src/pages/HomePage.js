a08f0bce921b4079847e76443f8a6a3105165750
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-04 20:44:42 -0600

    Addd prop validation in Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
 
@@ -9,5 +11,17 @@ function HomePage({ books, onDelete, onSubmit }) {
     </div>
   );
 }
+
+HomePage.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default HomePage;
