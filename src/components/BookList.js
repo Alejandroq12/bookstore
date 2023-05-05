de5dc4347ba54549b6685f1a9eac3ad988023369
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-04 20:44:08 -0600

    Addd prop validation in Home page

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import IndividualBook from './IndividualBook';
 
 function BookList({ books, onDelete }) {
   return (
     <div className="book-list">
-      {books.map((book, index) => (
-        <IndividualBook key={index} book={book} onDelete={onDelete} />
+      {books.map((book) => (
+        <IndividualBook key={book.id} book={book} onDelete={onDelete} />
       ))}
     </div>
   );
 }
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default BookList;
