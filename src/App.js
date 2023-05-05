e76d8213ea3fdfd70e8600960da9193cba542abe
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-04 20:19:59 -0600

    Set App.js to implement functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import Categories from './pages/Categories';
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  function addBook(newBook) {
+    const bookWithId = { ...newBook, id: Date.now() };
+    setBooks([...books, bookWithId]);
+  }
+
+  function deleteBook(bookId) {
+    setBooks(books.filter(book => book.id !== bookId));
+  }
+  
+
   return (
     <div className="App">
-      <h1>The Bookstore</h1>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage books={books} onDelete={deleteBook} onSubmit={addBook} />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
