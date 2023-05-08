c4fb865af2f88aadf2b0d9e8d1a0ab743138986e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-08 16:37:37 -0600

    Implement redux in the form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-function BookForm({ onSubmit }) {
+function BookForm() {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
 
   function handleSubmit(e) {
     e.preventDefault();
-    onSubmit({ title, author });
+    const newBook = {
+      item_id: Date.now().toString(),
+      title,
+      author,
+      category,
+    };
+    dispatch(addBook(newBook));
     setTitle('');
     setAuthor('');
+    setCategory('');
   }
 
   return (
@@ -27,13 +37,15 @@ function BookForm({ onSubmit }) {
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
       />
+      <input
+        type="text"
+        placeholder="Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
       <button type="submit">Add Book</button>
     </form>
   );
 }
-
-BookForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
 
 export default BookForm;
