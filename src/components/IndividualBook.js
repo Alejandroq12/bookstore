fa0a4095f5d3aecaacc463364c70bec71126dcf6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-09 15:46:35 -0600

    Add category

--- src/components/IndividualBook.js
+++ src/components/IndividualBook.js
@@ -1,34 +1,23 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { deleteBook } from '../redux/books/booksSlice';
-import ProgressCircle from './ProgressCircle';
 
-function IndividualBook({ book }) {
-  const dispatch = useDispatch();
+function IndividualBook({ book, onDelete }) {
+  const {
+    id, title, author, category,
+  } = book;
 
   const handleDelete = () => {
-    dispatch(deleteBook(book.id));
+    onDelete(id);
   };
 
   return (
-    <div className="individual-book">
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
-      <button type="button">Comments</button>
-      <button type="button" onClick={handleDelete}>
-        Remove
-      </button>
-      <button type="button">Edit</button>
-      <div className="progress">
-        <div className="circle" />
-        <ProgressCircle percentage={64} />
-        <span>64% Completed</span>
+    <div>
+      <div>
+        <p>{category}</p>
+        <h3>{title}</h3>
+        <p>{author}</p>
       </div>
-      <div className="current-chapter">
-        <h4>Current Chapter</h4>
-        <p>Chapter 3 of 5</p>
-        <button type="button">UPDATE PROGRESS</button>
-      </div>
+      <button type="submit" onClick={handleDelete}>Remove Book</button>
     </div>
   );
 }
@@ -38,7 +27,9 @@ IndividualBook.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default IndividualBook;
