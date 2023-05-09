ee2674a1756336bf7025067101544033962ea170
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-09 16:18:45 -0600

    Update structure to match design

--- src/components/IndividualBook.js
+++ src/components/IndividualBook.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ProgressCircle from './ProgressCircle';
 
 function IndividualBook({ book, onDelete }) {
   const {
@@ -16,8 +17,23 @@ function IndividualBook({ book, onDelete }) {
         <p>{category}</p>
         <h3>{title}</h3>
         <p>{author}</p>
+        <button type="button">Comments</button>
+        <button type="submit" onClick={handleDelete}>
+          Remove Book
+        </button>
+        <button type="button">Edit</button>
       </div>
-      <button type="submit" onClick={handleDelete}>Remove Book</button>
+
+      <div className="progress">
+        <div className="circle" />
+        <ProgressCircle percentage={64} />
+        <span>64% Completed</span>
+      </div>
+      <div className="current-chapter">
+        <h4>Current Chapter</h4>
+        <p>Chapter 3 of 5</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 }
