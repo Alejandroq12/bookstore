87e22722555e8611d16f00df5cfac10ce5aecc98
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-09 15:55:38 -0600

    Add header to API

--- src/api.js
+++ src/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
