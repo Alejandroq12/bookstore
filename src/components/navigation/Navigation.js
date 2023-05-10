4615be0eb0dc16e25bb890332858a12e224f4efb
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-10 13:38:46 -0600

    Add correct words to navigation bar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -8,8 +8,8 @@ function Navigation() {
     <nav className="navbar">
       <h1>Bookstore CMS</h1>
       <div className="nav-links">
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link categories" to="/categories">Categories</Link>
+        <Link className="nav-link" to="/">BOOKS</Link>
+        <Link className="nav-link categories" to="/categories">CATEGORIES</Link>
       </div>
       <FontAwesomeIcon className="icon" icon={faUserCircle} />
     </nav>
