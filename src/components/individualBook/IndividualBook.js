0c91dbeabae7533858c57bb58e64b1c40088d853
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-10 21:28:34 -0600

    Adjust Individual Book component to make styling seasier

--- src/components/individualBook/IndividualBook.js
+++ src/components/individualBook/IndividualBook.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ProgressCircle from '../ProgressCircle';
+import ProgressCircle from '../progress/ProgressCircle';
 import './IndividualBook.css';
 
 function IndividualBook({ book, onDelete }) {
@@ -20,19 +20,29 @@ function IndividualBook({ book, onDelete }) {
           <h3 className="book-title">{title}</h3>
           <p className="author">{author}</p>
           <div className="buttons">
-            <button className="comments" type="button">Comments</button>
+            <button className="comments" type="button">
+              Comments
+            </button>
             <button className="remove" type="button" onClick={handleDelete}>
               Remove Book
             </button>
-            <button className="edit" type="button">Edit</button>
+            <button className="edit" type="button">
+              Edit
+            </button>
           </div>
         </div>
 
         <div className="progress">
           <div className="circle" />
           <ProgressCircle percentage={64} />
-          <span>64% Completed</span>
+          <div className="percentage-text">
+            <div className="percentage-number">
+              <span>64%</span>
+            </div>
+            <span className="completed">Completed</span>
+          </div>
         </div>
+
         <div className="current-chapter">
           <h4>Current Chapter</h4>
           <p>Chapter 3 of 5</p>
